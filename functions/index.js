eb87f60027aa55f70f9658b07ec6c8f5a5927920
tsu-nera <fox10225fox@gmail.com>
2019-05-14 22:55:16 +0900

    temp

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,53 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express')
-const app = express();
+const config = {
+  apiKey: "AIzaSyC7MuTUeJi_nW-fLKALWfTbkiYoOzar44g",
+  authDomain: "socialage-8d7d4.firebaseapp.com",
+  databaseURL: "https://socialage-8d7d4.firebaseio.com",
+  projectId: "socialage-8d7d4",
+  storageBucket: "socialage-8d7d4.appspot.com",
+  messagingSenderId: "763760798484",
+  appId: "1:763760798484:web:45de67d70f11a317"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-  admin.firestore()
-  .collection('screams')
-  .orderBy('createdAt', 'desc')
-  .get()
-  .then(data => {
-    let screams = [];
-    data.forEach(doc => {
-      screams.push({
-        screamId: doc.id,
-        body: doc.data().body,
-        userHandle: doc.data().userHandle,
-        createdAt: doc.data().createdAt
+  db.collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount
+        });
       });
+      return res.json(screams);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
     });
-    return res.json(screams);
-  })
-  .catch(err => console.error(err))
-})
+});
 
+// Post one scream
 app.post('/scream', (req, res) => {
+  if (req.body.body.trim() === '') {
+    return res.status(400).json({ body: 'Body must not be empty' });
+  }
 
   const newScream = {
     body: req.body.body,
@@ -34,16 +55,64 @@ app.post('/scream', (req, res) => {
     createdAt: new Date().toISOString()
   };
 
-  admin.firestore()
-    .collection('screams')
+  db.collection('screams')
     .add(newScream)
-    .then(doc => {
-      res.json({message: `document ${doc.id} created successfully`})
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
     })
-    .catch(err => {
-      res.status(500).json( {error: 'sumething went wrong'} )
-      console.error(err)
-    })
-  })
+    .catch((err) => {
+      res.status(500).json({ error: 'something went wrong' });
+      console.error(err);
+    });
+});
 
-  exports.api = functions.https.onRequest(app);
+// Signup route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  // TODO: validate data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: 'this handle is already taken' });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already is use' });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+exports.api = functions.https.onRequest(app);
